801060f90273cc5baaa5f8eee1493781feb24877
eaa24martia <eaa24martia@gmail.com>
2025-12-07 14:15:33 +0100

    added svg images to the nav bar

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -3,14 +3,12 @@ import Image from "next/image";
 
 export default function BottomNav() {
   return (
-    <section className="fixed bottom-0 left-0 w-full bg-[#ffffff] z-50 shadow-lg rounded-[50px]">
-      <div className="flex justify-center">
-        <Image
-          src="/assets/red-logo.svg"
-          alt="Malling Bio Logo"
-          width={150}
-          height={90}
-        />
+    <section className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-[#ffffff] z-50 shadow-lg rounded-[50px]" style={{ width: '361px', height: '58px' }}>
+      <div className="flex justify-center items-center h-full">
+       <img src="/assets/house.svg" alt="Home page" />
+        <img src="/assets/events.svg" alt="Events page" />
+         <img src="/assets/tickets.svg" alt="Tickets page" />
+          <img src="/assets/profile.svg" alt="Profile page" />
       </div>
     </section>
   );
