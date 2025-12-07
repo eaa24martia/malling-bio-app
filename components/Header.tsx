3e9e9d806d79a808735d98cbc5b32c7f8173a7e1
eaa24martia <eaa24martia@gmail.com>
2025-12-07 10:20:48 +0100

    made the header sticky

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function RedHeader() {
   return (
-    <section className="w-full bg-[#B2182B]">
+    <section className="fixed top-0 left-0 w-full bg-[#B2182B] z-50">
       <div className="flex justify-center">
         <Image
           src="/assets/white-logo.svg"
