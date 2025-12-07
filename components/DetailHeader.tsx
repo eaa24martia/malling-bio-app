3c50f254fa0ec67cbf3213908e6d51b6a2691137
eaa24martia <eaa24martia@gmail.com>
2025-12-07 16:21:14 +0100

    added trailer button

--- components/DetailHeader.tsx
+++ components/DetailHeader.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CreateButton from "./CreateButton";
 
 export default function DetailHeader() {
   return (
@@ -11,8 +12,19 @@ export default function DetailHeader() {
         />
          <div className="absolute inset-0 bg-linear-to-b from-[#00000024] to-[#000000] pointer-events-none"></div>
 
+         <div className="absolute top-30 left-[-9%] z-10 scale-75">
+           <a
+             href="/trailer"
+             className="inline-block bg-[#B2182B] text-white font-bold px-8 py-3 rounded-full text-lg text-center shadow-[0_8px_0_0_#7C1C1A] active:translate-y-1 active:shadow-[0_4px_0_0_#7C1C1A]"
+           >
+             <div className="flex items-center gap-4 whitespace-nowrap">
+               Se trailer 
+               <img src="/assets/play.svg" alt="" className="w-6 h-6" />
+             </div>
+           </a>
+         </div>
 
-         <div className="absolute inset-0 flex items-end justify-center pb-8 z-20">
+         <div className="absolute inset-0 flex items-end justify-center pb-8 z-5">
            <div className="relative">
              <img 
                src="https://poster.ebillet.dk/WickedPartII-2025.large.jpg" 
