0c5e9095e7fcf419c9c176c65c5cce2ea25a763e
eaa24martia <eaa24martia@gmail.com>
2025-12-07 15:56:05 +0100

    added smaller poster on top of background

--- components/DetailHeader.tsx
+++ components/DetailHeader.tsx
@@ -10,6 +10,15 @@ export default function DetailHeader() {
           className="w-full h-full object-cover object-top"
         />
          <div className="absolute inset-0 bg-linear-to-b from-[#00000024] to-[#000000] pointer-events-none"></div>
+
+
+         <div className="absolute inset-0 flex items-end justify-center pb-8 z-20">
+           <img 
+             src="https://poster.ebillet.dk/WickedPartII-2025.large.jpg" 
+             alt="Movie Poster" 
+             className="w-[178px] h-[266px]object-cover rounded-lg shadow-lg"
+           />
+         </div>
     </section>
   );
 }       
