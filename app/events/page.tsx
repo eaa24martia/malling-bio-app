d0aa9bedffa51caccef193ef87b25db6c4ba7615
eaa24martia <eaa24martia@gmail.com>
2025-12-08 10:12:14 +0100

    added events page

---
+++ app/events/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import RedHeader from "@/components/Header";
+
+export default function EventsPage() {
+  return (
+
+    <main className="h-screen relative overflow-hidden"
+        style={{
+          backgroundImage: `url('assets/background-1.svg')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+        }}>
+
+        <section className="relative z-10">
+                  <RedHeader />
+        </section>
+
+        <div>
+           <h1 className="font-bold text-center text-[#192B5A] mb-0 mt-25" style={{ fontSize: '30px' }}>
+            Arrangementer
+          </h1>
+             <div className="h-0.5 bg-[#192B5A] my-4"></div>
+        </div>
+    </main>
+  );
+}
